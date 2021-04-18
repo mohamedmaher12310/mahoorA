3a9b18dde723561e14109b4cce0178f5aa0544ef
mahoora <mohamed.mohamed009@eng-st.cu.edu.eg>
2021-04-18 23:57:45 +0200

    dynamic array

--- main.c
+++ main.c
@@ -1,37 +1,14 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-struct Node {
-	int data;
-	struct Node* next;
-};
-int main()
-{
-	struct Node* head = NULL;
-	struct Node* second = NULL;
-	struct Node* third = NULL;
-
-	// allocate 3 nodes in the heap
-	head = (struct Node*)malloc(sizeof(struct Node));
-	second = (struct Node*)malloc(sizeof(struct Node));
-	third = (struct Node*)malloc(sizeof(struct Node));
-
-
-
-	head->data = 1;
-	head->next = second;
-
-	second->data = 2;
-
-
-	second->next = third;
-
-
-
-	third->data = 3; // assign data to third node
-	third->next = NULL;
-
-
-
-	return 0;
+#include<stdio.h>
+int main(void){
+int size;
+printf("\nEnter the size of the array: ");
+scanf("%d",&size);
+int *A;
+A=(int*)malloc(size*sizeof(int));
+printf("\nEnter the values of the array: ");
+for(int i=0;i<size;i++)
+    scanf("%d",&A[i]);
+printf("\nThe values of the array are: ");
+for(int i=0;i<size;i++)
+    printf("%d\t",A[i]);
 }
