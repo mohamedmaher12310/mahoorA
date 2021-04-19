643507a395d6c99f7485ce19b16aff5d7c1603d7
mahoora <mohamed.mohamed009@eng-st.cu.edu.eg>
2021-04-19 21:01:23 +0200

    second day

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-#include "Untitled1.c"
+#include<stdlib.h>
 struct student{
         int userID;
         char Name[25];
@@ -7,10 +7,20 @@ struct student{
         int  score;
 };
 
-int main(void){
-printf("Enter the number of students\n");
-int N;
-scanf("%d",&N);
-struct student s[N];
+int main(){
 
+struct student *ptr;
+int N;
+printf("Enter the number of students :\n");
+scanf("%d",&N);
+ptr=(struct student *)malloc(N*sizeof(struct student));
+for(int i=0;i<N;i++){
+    printf("Enter the data of student no. %d\n ID then Name then birthday then score\n",i+1);
+    scanf("%d %s %d %d",&(ptr+i)->userID,&(ptr+i)->Name[25],&(ptr+i)->birthday,&(ptr+i)->score);
+}
+
+
+
+
+return 0;
 }
